ec2f90b14afc4a6ef7ef266f199ab6b329c0048a
Ronc122 <cajanr02@gmail.com>
2019-01-17 23:08:11 +0800

    Redesign homepage

--- index.php
+++ index.php
@@ -1,25 +1,28 @@
 <?php include('server/connection.php'); ?>
+<?php include('login.php');?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Point of Sale</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" type="text/css" href="bootstrap4/css/bootstrap.min.css" />
-	<link rel="stylesheet" type="text/css" href="bootstrap4/css/style.css" />
+	<?php include('templates/head.php'); ?>
 </head>
 <body class="bg-dark">
 	<div class="text-center">
 		<div class="main h-100">
 			<img class="img-fluid" src="images/logo.png"/>
+  			<center><?php include('error.php');?></center>
 		</div>
 		<div class="fixed-bottom mb-2">
-			<button type="button" id="admin" name="admin" class="btn-lg btn-secondary" data-toggle="modal" data-target=".bd-example-modal-sm">Administrator</button>
-			<button type="button" id="user" class="btn-lg btn-secondary" data-toggle="modal" data-target="#modal-user">Log In</button>
+			<div class="d-inline">
+				<button type="button" id="admin" class="btn-lg btn-secondary" data-toggle="modal" data-target=".bd-example-modal-sm">Administrator</button>
+			</div>
+			<div class="d-inline">
+				<button type="button" id="user" class="btn-lg btn-secondary" data-toggle="modal" data-target="#modal-user">Log In</button>
 			</div>
 		</div>
+	</div>
 	<script src="bootstrap4/jquery/jquery.js"></script>
 	<script src="bootstrap4/js/bootstrap.bundle.min.js"></script>
+	<?php include('modals/admin_login_modal.php');?>
+	<?php include('modals/employee_login_modal.php');?>
 </body>
 </html>
-<?php include('modals/modals.php');?>
