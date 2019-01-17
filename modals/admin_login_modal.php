762a4d1061137a5ac4c3745d1a11d1f1fceaf204
Ronc122 <cajanr02@gmail.com>
2019-01-17 23:09:20 +0800

    Separating Modal for Admin login and Regular user

---
+++ modals/admin_login_modal.php
@@ -0,0 +1,22 @@
+<div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+	<div class="modal-dialog modal-sm modal-dialog-centered">
+		<div class="modal-content text-center">
+			<div class="modal-header">
+				<h5 class="modal-title">Sign in as Admin</h5>
+			</div>
+			<form method="post" action="">
+			<div class="modal-body">
+				<div>
+					<input type="hidden" name="username"/>
+					<input type="hidden" name="position" value="admin"/>
+					<input class="form-control-sm mb-2" type="password" name="password" placeholder="Enter Password" required/>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<button type="submit" name="login" class="btn btn-success">login</button>
+				</div>
+			</div>
+			</form>
+		</div>
+	</div>
+</div>
---
+++ modals/employee_login_modal.php
@@ -0,0 +1,22 @@
+<div class="modal fade" id="modal-user" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+	<div class="modal-dialog modal-sm modal-dialog-centered">
+		<div class="modal-content text-center">
+			<div class="modal-header">
+				<h5 class="modal-title">Sign In</h5>
+			</div>
+			<form method="post" action="">
+			<div class="modal-body">
+				<div>
+					<input type="hidden" name="position"/>
+					<input class="form-control-sm mb-2" type="text" name="username" placeholder="Enter Username" required/><br>
+					<input class="form-control-sm mb-2" type="password" name="password" placeholder="Enter Password" required/>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<button type="submit" name='login' class="btn btn-success">login</button>					
+				</div>
+			</form>
+			</div>
+		</div>
+	</div>
+</div>
