374884189d71ccccbdca1a017ea4f228a5c00744
Ronc122 <cajanr02@gmail.com>
2019-01-17 23:12:33 +0800

    Templates added

---
+++ templates/head.php
@@ -0,0 +1,6 @@
+<title>POS.PHP</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="icon" type="image/png" sizes="180x180" href="images/icon.png"/>
+<link rel="stylesheet" type="text/css" href="bootstrap4/css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="bootstrap4/css/style.css" />
