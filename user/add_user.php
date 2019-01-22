c784fc22f41d2412787d15a89ab19235026d2cf7
Ronc122 <cajanr02@gmail.com>
2019-01-22 21:29:15 +0800

    password input added

--- user/add_user.php
+++ user/add_user.php
@@ -1,4 +1,4 @@
-<?php include("../server/connection.php");?>
+<?php include('../user/add.php');?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,39 +16,49 @@
 				<div style="border:1px dashed black; width: 250px;height: 250px;">
 					<img class="img-fluid p-2 h-100 w-100" src="../images/contact.png">
 				</div>
-				<input class="form-control-sm" type="file" name="image">
+			<form method="post" enctype="multipart/form-data">
+				<input type="hidden" name="size" class="form-control-sm" value="1000000">
+				<input class="form-control-sm" type="file" name="image" required>
+				<p class="bg-danger mt-3"><?php echo $msg;?></p>
 			</div>
 			<dir class="second_side">
-				<form class="mt-5 ml-5 w-75" method="post">
-					<table>
+					<table class="table-responsive mt-5">
+						<p style="position: absolute; margin-top: 20px;"><?php include('../error.php');?></p>
 						<tbody>
 							<tr>
 								<td  valign="baseline">Username:</td>
-								<td class="pl-5 pb-4"><input type="text" name="username" value="" required></td>
+								<td class="pl-5 pb-2"><input type="text" name="username" required></td>
 							</tr>
 							<tr>
 								<td  valign="baseline">Firstname:</td>
-								<td class="pl-5 pb-4"><input type="text" name="firstname" value="" required></td>
+								<td class="pl-5 pb-2"><input type="text" name="firstname" required></td>
 							</tr>
 							<tr>
 								<td  valign="baseline">Lastname:</td>
-								<td class="pl-5 pb-4"><input type="text" name="lastname" value="" required></td>
+								<td class="pl-5 pb-2"><input type="text" name="lastname" required></td>
 							</tr>
 							<tr>
 								<td  valign="baseline">Contact number:</td>
-								<td class="pl-5 pb-4"><input type="text" name="number" value="" required></td>
+								<td class="pl-5 pb-2"><input type="number" name="number" required></td>
+							</tr>
+							<tr>
+								<td  valign="baseline">Password:</td>
+								<td class="pl-5 pb-2"><input type="password" name="password" required></td>
+							</tr>
+							<tr>
+								<td  valign="baseline">Confirm Password:</td>
+								<td class="pl-5 pb-2"><input type="password" name="password1" required></td>
 							</tr>
 							<tr>
 								<td  valign="baseline">Position:</td>
-								<td class="pl-5 pb-4"><select name="position" class="form-control-sm" required>
-									<option value="admin">admin</option>
-									<option value="employee">employee</option></td>
+								<td class="pl-5 pb-2"><input class="form-check-input ml-2" type="radio" name="position" value="admin"><label class="ml-4">Admin</label>
+									<input class="form-check-input ml-2" type="radio" name="position" value="employee" checked><label class="ml-4">Employee</label></td>
 							</tr>
 						</tbody>
 					</table>
-					<div class="text-left mt-5">
-						<button type="submit" class="btn btn-secondary">Submit</button>
-						<button class="btn btn-danger" onclick="window.location.href='../user/user.php?username='" >Cancel</button>
+					<div class="text-left mt-4">
+						<button type="submit" name="add" class="btn btn-secondary">Submit</button>
+						<button class="btn btn-danger" onclick="window.location.href='../user/user.php?username=<?php echo $_GET['username'	];?>'" >Cancel</button>
 					</div>
 				</form>
 			</dir>
