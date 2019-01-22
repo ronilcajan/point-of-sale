e9fd43879b27f415278997c4d5db3af61abcdf11
Ronc122 <cajanr02@gmail.com>
2019-01-22 21:28:10 +0800

    fix session start warning

--- server/connection.php
+++ server/connection.php
@@ -1,5 +1,4 @@
 <?php
-
 $database	= 'PointOfSale';
 $username	= 'root';
 $host		= 'localhost';
@@ -9,4 +8,14 @@ $db 	= mysqli_connect($host,$username,$password,$database);
 
 if($db == false){
 	die("Connection Failed: ".mysql_connect_error());
+}
+
+if (isset($_GET['logout'])){
+	session_destroy();
+	unset($_SESSION['username']);
+	header('location: index.php');
+}
+
+if (!isset($_SESSION)){
+	session_start();	
 }
