24be39d2c1114e9b38debdb803e397d2e7072cc4
ronilcajan <bugsgamer00@gmail.com>
2020-03-30 22:29:04 +0800

    Update Database

--- PointOfSale.sql
+++ PointOfSale.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 23, 2019 at 08:08 AM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.0
+-- Generation Time: Mar 30, 2020 at 04:26 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,10 +30,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `cashflow` (
   `transaction_id` int(11) NOT NULL,
-  `description` text,
+  `description` text DEFAULT NULL,
   `amount` decimal(18,2) DEFAULT NULL,
   `username` varchar(30) DEFAULT NULL,
-  `transaction_date` datetime DEFAULT CURRENT_TIMESTAMP
+  `transaction_date` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -53,7 +53,7 @@ CREATE TABLE `customer` (
   `customer_id` int(11) NOT NULL,
   `firstname` varchar(30) DEFAULT NULL,
   `lastname` varchar(30) DEFAULT NULL,
-  `address` text,
+  `address` text DEFAULT NULL,
   `contact_number` varchar(30) DEFAULT NULL,
   `image` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -75,7 +75,7 @@ CREATE TABLE `delivery` (
   `transaction_no` varchar(20) NOT NULL,
   `supplier_id` int(11) DEFAULT NULL,
   `username` varchar(20) DEFAULT NULL,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -85,7 +85,9 @@ CREATE TABLE `delivery` (
 INSERT INTO `delivery` (`transaction_no`, `supplier_id`, `username`, `date`) VALUES
 ('5CAAFDA8CD697', 21, 'admin', '2019-04-08 15:52:40'),
 ('5CAAFDEEDB333', 22, 'admin', '2019-04-08 15:54:19'),
-('5CAAFE37D21E8', 21, 'admin', '2019-04-08 15:55:28');
+('5CAAFE37D21E8', 21, 'admin', '2019-04-08 15:55:28'),
+('5E7F00084C934', 22, 'admin', '2020-03-28 15:43:22'),
+('5E81DF2B7B8F7', 22, 'admin', '2020-03-30 20:00:48');
 
 -- --------------------------------------------------------
 
@@ -110,7 +112,9 @@ INSERT INTO `initial_products` (`id`, `initial_quantity`) VALUES
 ('2', 200),
 ('3', 150),
 ('4', 125),
-('5', 100);
+('5', 100),
+('23213', 23),
+('10000', 21);
 
 -- --------------------------------------------------------
 
@@ -122,7 +126,7 @@ CREATE TABLE `logs` (
   `id` int(11) NOT NULL,
   `username` varchar(30) NOT NULL,
   `purpose` varchar(30) NOT NULL,
-  `logs_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `logs_time` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -144,7 +148,31 @@ INSERT INTO `logs` (`id`, `username`, `purpose`, `logs_time`) VALUES
 (864, 'admin', 'User admin logout', '2019-04-08 15:57:38'),
 (865, 'admin', 'User admin login', '2019-04-08 16:06:54'),
 (866, 'admin', 'User admin login', '2019-04-08 20:28:36'),
-(867, 'admin', 'Product sold', '2019-04-08 20:29:27');
+(867, 'admin', 'Product sold', '2019-04-08 20:29:27'),
+(868, 'admin', 'User admin login', '2020-03-23 16:04:06'),
+(869, 'admin', 'User admin logout', '2020-03-23 16:04:24'),
+(870, 'admin', 'User admin login', '2020-03-28 12:58:34'),
+(871, 'admin', 'User admin logout', '2020-03-28 13:02:20'),
+(872, 'admin', 'User admin login', '2020-03-28 13:02:26'),
+(873, 'admin', 'User admin logout', '2020-03-28 13:02:59'),
+(874, 'admin', 'User admin login', '2020-03-28 13:05:48'),
+(875, 'admin', 'Product sold', '2020-03-28 14:06:26'),
+(876, 'admin', 'Product sold', '2020-03-28 14:07:27'),
+(877, 'admin', 'Product sold', '2020-03-28 14:08:09'),
+(878, 'admin', 'Product sold', '2020-03-28 14:14:46'),
+(879, 'admin', 'Product sold', '2020-03-28 14:22:55'),
+(880, 'admin', 'Product sold', '2020-03-28 14:27:51'),
+(881, 'admin', 'Delivery Added', '2020-03-28 15:43:22'),
+(882, 'admin', 'Product sold', '2020-03-28 16:14:30'),
+(883, 'admin', 'User admin login', '2020-03-29 09:26:29'),
+(884, 'admin', 'User admin login', '2020-03-29 09:40:46'),
+(885, 'admin', 'Product Coffee updated', '2020-03-29 09:53:36'),
+(886, 'admin', 'Product Coffee updated', '2020-03-29 09:53:51'),
+(887, 'admin', 'User admin login', '2020-03-30 09:05:52'),
+(888, 'admin', 'Product sold', '2020-03-30 09:07:10'),
+(889, 'admin', 'User admin login', '2020-03-30 19:59:24'),
+(890, 'admin', 'Delivery Added', '2020-03-30 20:00:48'),
+(891, 'admin', 'User admin login', '2020-03-30 22:26:03');
 
 -- --------------------------------------------------------
 
@@ -159,6 +187,8 @@ CREATE TABLE `products` (
   `quantity` int(11) DEFAULT NULL,
   `unit` varchar(30) DEFAULT NULL,
   `min_stocks` int(11) DEFAULT NULL,
+  `remarks` text DEFAULT NULL,
+  `location` varchar(50) DEFAULT NULL,
   `images` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -166,15 +196,17 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`product_no`, `product_name`, `sell_price`, `quantity`, `unit`, `min_stocks`, `images`) VALUES
-('1', 'Coffee', '12.00', 70, 'sachet', 20, NULL),
-('1001', 'Glass', '22.00', 100, 'Box', 20, NULL),
-('10011', 'Chair', '600.00', 198, 'Each', 20, NULL),
-('10012', 'Sofa', '2400.00', 100, 'Each', 20, NULL),
-('2', 'Tooth Paste', '24.00', 200, 'sachet', 10, NULL),
-('3', 'Shampoo', '6.60', 150, 'sachet', 20, NULL),
-('4', 'Soap', '17.25', 125, 'sachet', 20, NULL),
-('5', 'Conditioner', '12.00', 100, 'sachet', 10, NULL);
+INSERT INTO `products` (`product_no`, `product_name`, `sell_price`, `quantity`, `unit`, `min_stocks`, `remarks`, `location`, `images`) VALUES
+('1', 'Coffee', '12.00', 70, 'sachet', 20, 'sfdsfdsf', 'fsdfdsfdsfds', NULL),
+('10000', 'asdas', '25575.60', 21, 'KA', 29, NULL, NULL, NULL),
+('1001', 'Glass', '22.00', 100, 'Box', 20, NULL, NULL, NULL),
+('10011', 'Chair', '600.00', 198, 'Each', 20, NULL, NULL, NULL),
+('10012', 'Sofa', '2400.00', 92, 'Each', 20, NULL, NULL, NULL),
+('2', 'Tooth Paste', '24.00', 200, 'sachet', 10, NULL, NULL, NULL),
+('23213', 'sdfsd', '42.24', 19, 'sdfsdf', 23, 'fdsfds', 'dffdf', NULL),
+('3', 'Shampoo', '6.60', 148, 'sachet', 20, NULL, NULL, NULL),
+('4', 'Soap', '17.25', 122, 'sachet', 20, NULL, NULL, NULL),
+('5', 'Conditioner', '12.00', 100, 'sachet', 10, NULL, NULL, NULL);
 
 --
 -- Triggers `products`
@@ -214,7 +246,9 @@ INSERT INTO `product_delivered` (`transaction_no`, `product_id`, `total_qty`, `b
 ('5CAAFE37D21E8', '2', 200, '20.00', 20),
 ('5CAAFE37D21E8', '3', 150, '6.00', 10),
 ('5CAAFE37D21E8', '4', 125, '15.00', 15),
-('5CAAFE37D21E8', '5', 100, '10.00', 20);
+('5CAAFE37D21E8', '5', 100, '10.00', 20),
+('5E7F00084C934', '23213', 23, '32.00', 32),
+('5E81DF2B7B8F7', '10000', 21, '21313.00', 20);
 
 -- --------------------------------------------------------
 
@@ -224,18 +258,28 @@ INSERT INTO `product_delivered` (`transaction_no`, `product_id`, `total_qty`, `b
 
 CREATE TABLE `sales` (
   `reciept_no` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL DEFAULT '0',
+  `customer_id` int(11) NOT NULL DEFAULT 0,
   `username` varchar(30) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `discount` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `sales`
 --
 
-INSERT INTO `sales` (`reciept_no`, `customer_id`, `username`, `date`) VALUES
-(20, 16, 'admin', '2019-04-08 07:56:39'),
-(21, 16, 'admin', '2019-04-08 12:29:27');
+INSERT INTO `sales` (`reciept_no`, `customer_id`, `username`, `discount`, `total`, `date`) VALUES
+(20, 16, 'admin', 0, 0, '2019-04-08 07:56:39'),
+(21, 16, 'admin', 0, 0, '2019-04-08 12:29:27'),
+(22, 16, 'admin', 0, 0, '2020-03-28 06:06:26'),
+(23, 16, 'admin', 0, 0, '2020-03-28 06:07:27'),
+(24, 16, 'admin', 0, 0, '2020-03-28 06:08:08'),
+(25, 16, 'admin', 10, 0, '2020-03-28 06:14:46'),
+(26, 16, 'admin', 10, 0, '2020-03-28 06:22:55'),
+(27, 16, 'admin', 10, 2160, '2020-03-28 06:27:51'),
+(28, 16, 'admin', 20, 1920, '2020-03-28 08:14:30'),
+(29, 16, 'admin', 20, 4017, '2020-03-30 01:07:10');
 
 -- --------------------------------------------------------
 
@@ -257,7 +301,17 @@ CREATE TABLE `sales_product` (
 INSERT INTO `sales_product` (`reciept_no`, `product_id`, `price`, `qty`) VALUES
 (20, '1', '12.00', 10),
 (20, '10011', '600.00', 2),
-(21, '1', '12.00', 20);
+(21, '1', '12.00', 20),
+(22, '10012', '2400.00', 1),
+(23, '3', '6.60', 2),
+(24, '10012', '2400.00', 1),
+(25, '10012', '2400.00', 1),
+(26, '10012', '2400.00', 1),
+(27, '10012', '2400.00', 1),
+(28, '10012', '2400.00', 1),
+(29, '10012', '2400.00', 2),
+(29, '4', '17.25', 3),
+(29, '23213', '42.24', 4);
 
 -- --------------------------------------------------------
 
@@ -270,7 +324,7 @@ CREATE TABLE `supplier` (
   `company_name` varchar(30) DEFAULT NULL,
   `firstname` varchar(30) DEFAULT NULL,
   `lastname` varchar(30) DEFAULT NULL,
-  `address` text,
+  `address` text DEFAULT NULL,
   `contact_number` varchar(30) DEFAULT NULL,
   `image` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -305,7 +359,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `firstname`, `lastname`, `position`, `contact_number`, `image`, `password`) VALUES
-(7, 'admin', 'Juan', 'Cruz', 'admin', '+63(09)1234-1234', 'Myprofile.jpg', '2637a5c30af69a7bad877fdb65fbd78b'),
+(7, 'admin', 'Juan', 'Cruz', 'admin', '+63(09)1234-1234', 'Myprofile.jpg', '21232f297a57a5a743894a0e4a801fc3'),
 (13, 'employ1', 'Chris', 'Doe', 'Employee', '+63(09)1234-1234', 'men-in-black.png', '7566c2ba785a2cb830c4edc1f5f70dec');
 
 --
@@ -407,19 +461,19 @@ ALTER TABLE `customer`
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=868;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=892;
 
 --
 -- AUTO_INCREMENT for table `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `reciept_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `reciept_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `sales_product`
 --
 ALTER TABLE `sales_product`
-  MODIFY `reciept_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `reciept_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `supplier`
@@ -431,7 +485,7 @@ ALTER TABLE `supplier`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
