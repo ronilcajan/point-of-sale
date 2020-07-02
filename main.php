fa6a08b4e51657e8ffc1fd83a4163d8b2013ca57
ronilcajan <bugsgamer00@gmail.com>
2020-07-02 09:43:25 +0800

    fix discount error if empty value

--- main.php
+++ main.php
@@ -82,7 +82,7 @@
 				<small>
 					<ul class="text-white justify-content-center">
 						<li class="d-flex mb-0">Total (₱): <p id="totalValue1" class="mb-0 ml-5 pl-3">0.00</p></li>
-						<li class="mb-0 mt-0">Discount (₱): <input style="width: 100px" class="text-right" type="number" name="discount" value="" min="1" placeholder="Enter Discount" id="discount"></li>
+						<li class="mb-0 mt-0">Discount (₱): <input style="width: 100px" class="text-right form-control-sm" type="number" name="discount" value="0" min="0" placeholder="Enter Discount" id="discount" ></li>
 					</ul>
 				</small>
 				</div>
